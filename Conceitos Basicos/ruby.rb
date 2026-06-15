8f395df71481e53de65617842103b32c8e406cdd
sistemas-iberika <168090412+ryan-ggarcia@users.noreply.github.com>
2026-06-15 17:01:26 -0300

    Primeiros passos

---
+++ Conceitos Basicos/ruby.rb
@@ -0,0 +1,20 @@
+# Tipagem dinamica
+int = 10
+float = 1.1
+string = "Olá"
+multiple = [1,2]
+
+# Simbolos são variavels que armazenam valores em um unico lugar
+Symbol = :exp
+
+# tipo Hash é igual a uma array mas não precisamos chamar a posição e sim o nome que esta apontado/ conjunto de simbolos
+
+oneHash = {course:'ruby',languege: 'Pt-br',name: 'Ryan'}
+oneHash[:couse]
+
+# Saida de dados
+print 'Digite seu nome:'
+# Salvando o valor
+name= gets.chomp
+
+puts "Hello #{nome}!"
